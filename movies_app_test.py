6792bf89bb5aa43a6bc8de0682a261b716677862
Kit Sum Pang <kitsumpang@gmail.com>
2019-04-27 12:00:59 +0100

    changed function name and switch string interpolation for formatting

--- movies_app_test.py
+++ movies_app_test.py
@@ -2,8 +2,8 @@ from movies_app import Movies
 
 def test_return_blank_statement():
 	movie = Movies("Jim Bob")
-	assert movie.statement() == "Rental Record For Jim Bob \n You Owe 0.0 \n You earned 0"
+	assert movie.get_statement() == "Rental Record For Jim Bob \n You Owe 0.0 \n You earned 0"
 
 def test_return_customer2_statement():
 	movie = Movies("Sally Burns")
-	assert movie.statement() == "Rental Record For Sally Burns \n You Owe 0.0 \n You earned 0"
+	assert movie.get_statement() == "Rental Record For Sally Burns \n You Owe 0.0 \n You earned 0"
