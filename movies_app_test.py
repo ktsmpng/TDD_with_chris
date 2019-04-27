45d92927c697afb6d679e691d7ad7d607f8bfef6
Kit Sum Pang <kitsumpang@gmail.com>
2019-04-27 15:21:11 +0100

    adding multiple movies

--- movies_app_test.py
+++ movies_app_test.py
@@ -12,3 +12,9 @@ def test_return_customer1_statement_with_movie():
 	movie = Movies("Jim Bob")
 	movie.add_rental('Crazynotes')
 	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes \nYou owe 0.0 \nYou earned 0"
+
+def test_return_customer1_statement_with_multiple_movies():
+	movie = Movies("Jim Bob")
+	movie.add_rental('Crazynotes', 'Teeth')
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes \n Teeth \nYou owe 0.0 \nYou earned 0"
+	
