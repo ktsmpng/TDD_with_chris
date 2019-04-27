ec0f9f851a599858a461801999cd19a43d11328b
Kit Sum Pang <kitsumpang@gmail.com>
2019-04-27 12:00:59 +0100

    changed function name and switch string interpolation for formatting

--- movies_app.py
+++ movies_app.py
@@ -1,8 +1,8 @@
 class Movies:
-	_statement = f"Rental Record For {self.customer} \n You Owe 0.0 \n You earned 0"
+	_statement = 'Rental Record For {} \n You Owe 0.0 \n You earned 0'
 
 	def __init__(self, customer):
 		self.customer = customer
 
-	def statement(self):
-		return _statement
+	def get_statement(self):
+		return self._statement.format(self.customer)
