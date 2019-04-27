677dc2bc4d76b51876783445fd688e76972fcc84
Kit Sum Pang <kitsumpang@gmail.com>
2019-04-27 15:13:28 +0100

    add movies functionality

--- movies_app.py
+++ movies_app.py
@@ -2,7 +2,20 @@ class Movies:
 
 	def __init__(self, customer):
 		self.customer = customer
-		self._statement = 'Rental Record For {} \n You Owe 0.0 \n You earned 0'
+		self._statement = 'Rental Record For {} \n{}You owe 0.0 \nYou earned 0'
+		self.rented_movies = []
 
 	def get_statement(self):
-		return self._statement.format(self.customer)
+		rented_movies = ''
+		for movie in self.rented_movies:
+			rented_movies += f' {movie} \n'
+		try:
+			self._statement = self._statement.format(self.customer, rented_movies)
+		except:
+			self._statement = self._statement.format(self.customer, '')
+
+		print(self._statement)
+		return self._statement
+
+	def add_rental(self, movie):
+		self.rented_movies.append(movie)
--- movies_app_test.py
+++ movies_app_test.py
@@ -2,8 +2,13 @@ from movies_app import Movies
 
 def test_return_blank_statement():
 	movie = Movies("Jim Bob")
-	assert movie.get_statement() == "Rental Record For Jim Bob \n You Owe 0.0 \n You earned 0"
+	assert movie.get_statement() == "Rental Record For Jim Bob \nYou owe 0.0 \nYou earned 0"
 
 def test_return_customer2_statement():
 	movie = Movies("Sally Burns")
-	assert movie.get_statement() == "Rental Record For Sally Burns \n You Owe 0.0 \n You earned 0"
+	assert movie.get_statement() == "Rental Record For Sally Burns \nYou owe 0.0 \nYou earned 0"
+
+def test_return_customer1_statement_with_movie():
+	movie = Movies("Jim Bob")
+	movie.add_rental('Crazynotes')
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes \nYou owe 0.0 \nYou earned 0"
