b0bd55cfcaac5760e13064fbf0cd67211feaa1b3
Kit Sum Pang <kitsumpang@gmail.com>
2019-05-09 17:59:42 +0100

    working app with added points and total functionality

--- movies_app.py
+++ movies_app.py
@@ -2,8 +2,8 @@ class Movies:
 
 	def __init__(self, customer):
 		self.customer = customer
-		self._statement = 'Rental Record For {} \n{}You owe 0.0 \nYou earned 0'
-		self.rented_movies = []
+		self._statement = 'Rental Record For {} \n{}You owe {} \nYou earned {}'
+		self.rented_movies = {}
 		self.catalog = {'Crazynotes': 'Regular',
 						'Teeth': 'Regular',
 						'The Web': 'Regular',
@@ -12,25 +12,69 @@ class Movies:
 
 	def get_statement(self):
 		try:
-			self._statement = self._statement.format(self.customer, self.movies_with_price())
+			self._statement = self._statement.format(self.customer, self.movies_with_price(), self.calculate_total(), self.calculate_points())
 		except:
 			self._statement = self._statement.format(self.customer, '')
 
 		return self._statement
 
-	def add_rental(self, *movies):
-		for movie in movies:
-			self.rented_movies.append(movie)
+	def add_rental(self, **movies):
+		for movie, rented_days in movies.items():
+			self.rented_movies[movie] = rented_days
+
+	def rented_days(self, movie):
+		return self.rented_movies[movie]
 
 	def movies_with_price(self):
 		rented_movies = ''
 		for movie in self.rented_movies:
 			if self.catalog[movie] == 'Regular':
-				rented_movies += f' {movie} 2.0 \n'
+				rented_movies += f' {movie} {self.calculate_regular_movie_price(movie)} \n'
 			elif self.catalog[movie] == 'Child':
-				rented_movies += f' {movie} 1.5 \n'
+				rented_movies += f' {movie} {self.calculate_childs_movie_price(movie)} \n'
 			elif self.catalog[movie] == 'New Release':
-				rented_movies += f' {movie} 3.0 \n'
+				rented_movies += f' {movie} {self.calculate_new_release_movie_price(movie)} \n'
 		return rented_movies
+
+	def calculate_regular_movie_price(self, movie):
+		additional_days_price = (1.5 * (self.rented_days(movie) - 2))
+		price = 2.0 + additional_days_price if self.rented_days(movie) > 2 else 2.0 
+		return price
+
+	def calculate_childs_movie_price(self, movie):
+		additional_days_price = (1.5 * (self.rented_days(movie) - 3))
+		price = 1.5 + additional_days_price if self.rented_days(movie) > 3 else 1.5
+		return price
+
+	def calculate_new_release_movie_price(self, movie):
+		price = 3.0 * self.rented_days(movie)
+		return price
+
+	def calculate_total(self):
+		total = 0.0
+		for movie in self.rented_movies:
+			if self.catalog[movie] == 'Regular':
+				total += self.calculate_regular_movie_price(movie)
+			elif self.catalog[movie] == 'Child':
+				total += self.calculate_childs_movie_price(movie)
+			elif self.catalog[movie] == 'New Release':
+				total += self.calculate_new_release_movie_price(movie)
+		return total
+
+	def calculate_points(self):
+		total = 0
+		for movie in self.rented_movies:
+			if self.catalog[movie] == 'Regular':
+				total += 1
+			elif self.catalog[movie] == 'Child':
+				total += 1
+			elif self.catalog[movie] == 'New Release':
+				total += self.calculate_new_release_movie_points(movie)
+		return total
+
+	def calculate_new_release_movie_points(self, movie):
+		return 1 if self.rented_days(movie) == 1 else 2
+
+
 
 
--- movies_app_test.py
+++ movies_app_test.py
@@ -10,31 +10,62 @@ def test_return_customer2_statement():
 
 def test_return_list_of_customer_rented_movie():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes')
-	assert movie.rented_movies == ['Crazynotes']
+	movie.add_rental(Crazynotes = 1)
+	assert movie.rented_movies == {'Crazynotes': 1}
 
 def test_return_list_of_customer_rented_movies():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes', 'Teeth')
-	assert movie.rented_movies == ['Crazynotes', 'Teeth']
+	movie.add_rental(Crazynotes = 1, Teeth = 1)
+	assert movie.rented_movies == {'Crazynotes': 1, 'Teeth': 1}
 
 def test_return_customer_statement_with_regular_movie_and_its_price():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes')
-	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \nYou owe 0.0 \nYou earned 0"
+	movie.add_rental(Crazynotes = 1)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \nYou owe 2.0 \nYou earned 1"
 
 def test_return_customer_statement_with_regular_and_child_movie_and_its_price():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes', 'Spongebob')
-	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \n Spongebob 1.5 \nYou owe 0.0 \nYou earned 0"
+	movie.add_rental(Crazynotes = 1, Spongebob = 1)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \n Spongebob 1.5 \nYou owe 3.5 \nYou earned 2"
 
 def test_return_customer_statement_with_regular_and_child_and_new_movie_and_its_price():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes', 'Spongebob', 'Avengers')
-	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \n Spongebob 1.5 \n Avengers 3.0 \nYou owe 0.0 \nYou earned 0"
+	movie.add_rental(Crazynotes = 1, Spongebob = 1 , Avengers = 1)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \n Spongebob 1.5 \n Avengers 3.0 \nYou owe 6.5 \nYou earned 3"
 
 def test_return_customer_statement_with_regular_for_over_two_days_and_its_price():
 	movie = Movies("Jim Bob")
-	movie.add_rental('Crazynotes')
-	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 3.5 \nYou owe 0.0 \nYou earned 0"
+	movie.add_rental(Crazynotes = 2)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 2.0 \nYou owe 2.0 \nYou earned 1"
+
+def test_return_customer_statement_with_regular_for_over_three_days_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Crazynotes = 3)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Crazynotes 3.5 \nYou owe 3.5 \nYou earned 1"
+
+def test_return_customer_statement_with_childs_movie_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Spongebob = 1)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Spongebob 1.5 \nYou owe 1.5 \nYou earned 1"
+
+def test_return_customer_statement_with_childs_movie_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Spongebob = 4)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Spongebob 3.0 \nYou owe 3.0 \nYou earned 1"
+
+def test_return_customer_statement_with_new_release_movie_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Avengers = 1)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Avengers 3.0 \nYou owe 3.0 \nYou earned 1"
+
+
+def test_return_customer_statement_with_new_release_movie_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Avengers = 4)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Avengers 12.0 \nYou owe 12.0 \nYou earned 2"
+
+def test_return_customer_statement_with_new_release_movie_and_its_price():
+	movie = Movies("Jim Bob")
+	movie.add_rental(Avengers = 4, Spongebob = 8, Crazynotes = 10)
+	assert movie.get_statement() == "Rental Record For Jim Bob \n Avengers 12.0 \n Spongebob 9.0 \n Crazynotes 14.0 \nYou owe 35.0 \nYou earned 4"
 
